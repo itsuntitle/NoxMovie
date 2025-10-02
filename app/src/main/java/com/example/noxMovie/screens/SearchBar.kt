70aac311d88cb1684b0a4b60a460a567b208b94a
Your Name <you@example.com>
2025-10-02 17:40:32 +0330

    initial commit

--- app/src/main/java/com/example/noxMovie/screens/SearchBar.kt
+++ app/src/main/java/com/example/noxMovie/screens/SearchBar.kt
@@ -2,6 +2,7 @@ package com.example.noxMovie.screens
 
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
@@ -11,6 +12,9 @@ import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.OutlinedTextField
 import androidx.compose.material.Text
 import androidx.compose.material3.ElevatedCard
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.OutlinedTextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -18,6 +22,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.onFocusChanged
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -33,11 +38,24 @@ fun SearchBar(
 ) {
     var isFocused by remember { mutableStateOf(false) }
     var savedWordsState by remember { mutableStateOf(savedWords) }
+    val isDark = isSystemInDarkTheme()
+    val selectedColor = MaterialTheme.colorScheme.primary
 
-
+// رنگ متن و پس‌زمینه بر اساس دارک یا لایت مود
+    val textColor = if (isDark) Color.White else Color.Gray
+    val containerColor = if (isDark) Color.DarkGray.copy(alpha = 0.1f) else Color.LightGray.copy(alpha = 0.1f)
     Column(modifier = Modifier.padding(5.dp)) {
         OutlinedTextField(
             value = text,
+            colors = OutlinedTextFieldDefaults.colors(
+                focusedTextColor = textColor,
+                unfocusedTextColor = textColor,
+                focusedContainerColor = containerColor,
+                unfocusedContainerColor = textColor,
+                focusedBorderColor = selectedColor,
+                unfocusedBorderColor = selectedColor
+            ),
+
             onValueChange = onTextValueChange,
             keyboardOptions = KeyboardOptions(imeAction = ImeAction.Search),
             label = { Text("Search") },
